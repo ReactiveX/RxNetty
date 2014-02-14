89e8aecba545d51bcb2f959043a481ad558f6646
Nitesh Kant <nkant@netflix.com>
2014-02-14 01:09:44 -0800

    Updated Copyright

--- src/main/java/io/reactivex/netty/pipeline/PipelineConfigurator.java
+++ src/main/java/io/reactivex/netty/pipeline/PipelineConfigurator.java
@@ -1,12 +1,12 @@
-/**
- * Copyright 2013 Netflix, Inc.
- * 
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 package io.reactivex.netty.pipeline;
 
 import io.netty.channel.ChannelPipeline;
+import io.reactivex.netty.client.RxClientImpl;
 import io.reactivex.netty.server.RxServer;
 
 /**
@@ -23,11 +24,11 @@ import io.reactivex.netty.server.RxServer;
  *
  * @param <R> The input object type for the pipeline i.e. the object type which is read from the pipeline.
  *           This type is not enforced by the pipeline configurator per se, but they are just to make the creator of
- *           {@link RxServer} and {@link io.reactivex.netty.client.RxClientImpl} type aware.
+ *           {@link RxServer} and {@link RxClientImpl} type aware.
  *
  * @param <W> The output object type for the pipeline i.e. the object type which is written to the pipeline.
  *           This type is not enforced by the pipeline configurator per se, but they are just to make the creator of
- *           {@link RxServer} and {@link io.reactivex.netty.client.RxClientImpl} type aware.
+ *           {@link RxServer} and {@link RxClientImpl} type aware.
  */
 public interface PipelineConfigurator<R, W> {
 
