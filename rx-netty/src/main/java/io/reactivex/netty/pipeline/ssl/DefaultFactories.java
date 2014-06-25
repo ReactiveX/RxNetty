edaac475c8d291f72ad4ed0513835f3bf19dbffd
Tomasz Bak <tbak@netflix.com>
2014-06-24 17:08:03 -0700

    Add default implementation of SSLEngineFactory for javax.net.ssl.SSLContext.

--- rx-netty/src/main/java/io/reactivex/netty/pipeline/ssl/DefaultFactories.java
+++ rx-netty/src/main/java/io/reactivex/netty/pipeline/ssl/DefaultFactories.java
@@ -5,6 +5,7 @@ import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLException;
 import java.security.cert.CertificateException;
@@ -19,6 +20,24 @@ public final class DefaultFactories {
     public static SSLEngineFactory TRUST_ALL = new TrustAllSSLEngineFactory();
 
     private DefaultFactories() {
+    }
+
+    public static SSLEngineFactory fromSSLContext(SSLContext sslContext) {
+        return new FromSSLContext(sslContext);
+    }
+
+    public static class FromSSLContext implements SSLEngineFactory {
+
+        private final SSLContext sslContext;
+
+        public FromSSLContext(SSLContext sslContext) {
+            this.sslContext = sslContext;
+        }
+
+        @Override
+        public SSLEngine createSSLEngine(ByteBufAllocator allocator) {
+            return sslContext.createSSLEngine();
+        }
     }
 
     private static class TrustAllSSLEngineFactory implements SSLEngineFactory {
