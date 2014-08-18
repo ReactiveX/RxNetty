93e9f101ac64442859a525820caafb9122d347be
Nitesh Kant <nkant@netflix.com>
2014-08-18 16:34:03 -0700

    Added missing copyrights

--- rx-netty-servo/src/main/java/io/reactivex/netty/servo/http/websocket/WebSocketClientListener.java
+++ rx-netty-servo/src/main/java/io/reactivex/netty/servo/http/websocket/WebSocketClientListener.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.reactivex.netty.servo.http.websocket;
-
-import java.util.concurrent.TimeUnit;
 
 import com.netflix.servo.monitor.Counter;
 import com.netflix.servo.monitor.LongGauge;
@@ -10,8 +23,13 @@ import io.reactivex.netty.metrics.WebSocketClientMetricEventsListener;
 import io.reactivex.netty.protocol.http.websocket.WebSocketClientMetricsEvent;
 import io.reactivex.netty.servo.tcp.TcpClientListener;
 
-import static com.netflix.servo.monitor.Monitors.*;
-import static io.reactivex.netty.servo.ServoUtils.*;
+import java.util.concurrent.TimeUnit;
+
+import static com.netflix.servo.monitor.Monitors.newCounter;
+import static com.netflix.servo.monitor.Monitors.newTimer;
+import static io.reactivex.netty.servo.ServoUtils.decrementLongGauge;
+import static io.reactivex.netty.servo.ServoUtils.incrementLongGauge;
+import static io.reactivex.netty.servo.ServoUtils.newLongGauge;
 
 /**
  * @author Tomasz Bak
