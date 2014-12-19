bf36f3319d031a47375cc00f39eb05d5d3142c9a
Ben Christensen <benjchristensen@netflix.com>
2014-12-18 20:05:57 -0800

    Revert Test Code

--- rxnetty-examples/src/main/java/io/reactivex/netty/examples/http/chunk/HttpChunkClient.java
+++ rxnetty-examples/src/main/java/io/reactivex/netty/examples/http/chunk/HttpChunkClient.java
@@ -105,12 +105,11 @@ public class HttpChunkClient {
     }
 
     public static void main(String[] args) {
-        //        if (args.length < 1) {
-        //            System.err.println("ERROR: give 'word' parameter");
-        //            System.exit(-1);
-        //        }
-        //        String word = args[0];
-        String word = "public";
+        if (args.length < 1) {
+            System.err.println("ERROR: give 'word' parameter");
+            System.exit(-1);
+        }
+        String word = args[0];
         System.out.println("Sending GET request...");
         System.out.printf("Counted %d words '%s'", new HttpChunkClient(DEFAULT_PORT).filterWords(word), word);
     }
